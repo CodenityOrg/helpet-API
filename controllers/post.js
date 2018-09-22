addc43804476bcdc468daa88c5b403ba0815b162
Angel <aleangrodriguez123@gmail.com>
2018-09-22 10:15:04 -0500

    Updated seeders

--- controllers/post.js
+++ controllers/post.js
@@ -131,6 +131,8 @@ module.exports = {
                 race: 1, 
                 description: 1,
                 date: 1,
+                latitude:1,
+                longitude:1,
                 photos: 1,
                 address: 1
             }
--- seeders/FeatureSeeder.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const faker = require("faker");
-const Bale = require("bale.js");
-const bale = new Bale();
-
-module.exports = bale.genSeed("features", 10, (feature) =>{
-    feature.name = faker.lorem.word();
-    return feature;
-}, "posts");
