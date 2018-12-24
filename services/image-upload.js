e899e5c80141f9800d7a89a23b1ff43ecd77efe8
Angel Rodriguez <aleangrodriguez123@gmail.com>
2018-12-24 15:20:26 -0500

    aws config

--- services/image-upload.js
+++ services/image-upload.js
@@ -2,13 +2,7 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const as3 = require("../as3");
-
-aws.config.update({
-  secretAccessKey: as3.secretAccessKey,
-  accessKeyId: as3.accessKeyId,
-  region: as3.region
-});
-
+aws.config.update(as3);
 const s3 = new aws.S3();
 
 const fileFilter = (req, file, cb) => {
@@ -19,12 +13,13 @@ const fileFilter = (req, file, cb) => {
   }
 }
 
-const upload = multer({
-  fileFilter,
-  storage: multerS3({
+let storage
+
+if (process.env.NODE_ENV === "production") {
+  storage = multerS3({
     acl: 'public-read',
     s3,
-    bucket: 'helpetdb',
+    bucket: 'helpet-bucket',
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
     },
@@ -32,6 +27,20 @@ const upload = multer({
       cb(null, Date.now().toString())
     }
   })
+} else {
+  storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/helpet-uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  })
+}
+
+const upload = multer({
+  fileFilter,
+  storage
 });
 
 module.exports = upload;
