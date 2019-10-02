dff9c5378638e82edff358e56b0379b434533d4b
Angel <aleangrodriguez123@gmail.com>
2019-10-02 06:49:00 -0500

    add new seeder data for default posts

--- models/Tag.js
+++ models/Tag.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const tagSchema = new Schema({
-    value: String,
+    value: {
+        type: String,
+        unique: true
+    },
 })
 
 tagSchema.statics.findOrCreate = function (filter, model) {
