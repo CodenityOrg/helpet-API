00fe38885baf9f4fa3625329d0e4ccb8fa62eb31
CristianPeralta <cbperaltasegura@gmail.com>
2020-01-10 15:34:45 -0500

    lint models

--- models/User.js
+++ models/User.js
@@ -1,76 +1,79 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
 const bcrypt = require("bcryptjs");
+
+const { Schema } = mongoose;
 const saltRounds = 10;
+
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email:{
-        type: String,
-        unique: true,
-        required: true
-    },
-    profile: {
-        type: String,
-        default: "https://i2.wp.com/drogaspoliticacultura.net/wp-content/uploads/2017/09/placeholder-user.jpg"
-    },
-    isVerified: {
-        type: Boolean,
-        default: false
-    },
-    token: {
-        type: String,
-        default: "secret"
-    },
-    phone: {
-        type: String
-    },
-    accessToken: {
-        type: String
-    },
-    password: String,
-    receiverId: String
-})
-
-const generateHash = function (password) {
-    const salt = bcrypt.genSaltSync(saltRounds);
-	return bcrypt.hashSync(password, salt);
-}
-
-userSchema.pre('save', function(next) {
-    if (this.password) {
-    	this.password = generateHash(this.password);
-    }
-	next();
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  profile: {
+    type: String,
+    default:
+      "https://i2.wp.com/drogaspoliticacultura.net/wp-content/uploads/2017/09/placeholder-user.jpg"
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  token: {
+    type: String,
+    default: "secret"
+  },
+  phone: {
+    type: String
+  },
+  accessToken: {
+    type: String
+  },
+  password: String,
+  receiverId: String
 });
 
-userSchema.statics.findOrCreate = async function (args, filter) {
-    try {
-        let user = await this.findOne(filter);
-        if (!user) {
-            user = await this.create(args);
-        }
-        return user;
-    } catch (error) {
-        console.log(error);
+const generateHash = function(password) {
+  const salt = bcrypt.genSaltSync(saltRounds);
+  return bcrypt.hashSync(password, salt);
+};
+
+userSchema.pre("save", function(next) {
+  if (this.password) {
+    this.password = generateHash(this.password);
+  }
+  next();
+});
+
+userSchema.statics.findOrCreate = async function(args, filter) {
+  try {
+    let user = await this.findOne(filter);
+    if (!user) {
+      user = await this.create(args);
     }
-}
+    return user;
+  } catch (error) {
+    return console.log(error);
+  }
+};
 
 userSchema.statics.generateHash = generateHash;
 
-userSchema.statics.login = async function (email, password) {
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email }).exec();
+  if (!user) {
+    return null;
+  }
 
-    const user = await this.findOne({ email }).exec();
-    if (!user) {
-        return null;
-    }
+  const comp = bcrypt.compareSync(password, user.password);
+  if (comp) {
+    delete user.password;
+    user.logged = comp;
+    return user;
+  }
+  return null;
+};
 
-    const comp = bcrypt.compareSync(password, user.password);
-    if (comp) {
-        delete user.password;
-        user.logged = comp;
-        return user;
-    }
-}
-
-module.exports = mongoose.model('User',userSchema);
+module.exports = mongoose.model("User", userSchema);
