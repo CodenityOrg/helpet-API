e0132db5135ebe575b7a2cdb8318cf566224207a
Angel <aleangrodriguez123@gmail.com>
2020-01-20 10:52:42 -0500

    change to uppername

--- deploy/production.js
+++ deploy/production.js
@@ -1,10 +1,10 @@
 module.exports = {
     "port": process.env.port,
     "db": {
-        "host": process.env.dbhost,
-        "name": process.env.dbname,
-        "port": process.env.dbport,
-        "user": process.env.dbuser,
-        "password": process.env.dbpassword
+        "host": process.env.DBHOST,
+        "name": process.env.DBNAME,
+        "port": process.env.DBPORT,
+        "user": process.env.DBUSER,
+        "password": process.env.DBPASSWORD
     }
 }
