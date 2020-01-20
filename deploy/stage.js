5f34db0576b0234ddba415a1d60801549ef1d319
Angel <aleangrodriguez123@gmail.com>
2020-01-20 10:39:18 -0500

    reset env variables config

---
+++ deploy/stage.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "port": process.env.port,
+    "db": {
+        "host": process.env.dbhost,
+        "name": process.env.dbname,
+        "port": process.env.dbport,
+        "user": process.env.dbuser,
+        "password": process.env.dbpassword
+    }
+}
