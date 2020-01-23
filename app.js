73d2badcdcca9721adeb60fbaca2b16958b9b764
CristianPeralta <cbperaltasegura@gmail.com>
2020-01-23 15:58:13 -0500

    add hsts helmet middleware

--- app.js
+++ app.js
@@ -15,11 +15,14 @@ connectDb();
 const app = express();
 app.use(cors());
 
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: "deny" }));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
