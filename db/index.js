da7b464f5e2e8532dc0b93bff3ea8471ef8006a2
Cristian Peralta <cbperaltasegura@gmail.com>
2023-10-24 14:51:48 -0500

    chore: Update Mongoose version and add database connection options

--- db/index.js
+++ db/index.js
@@ -1,16 +1,20 @@
 const mongoose = require("mongoose");
 
-const connect = () =>{
+const connect = () => {
+  const databaseUrl = ["production", "development"].includes(
+    process.env.NODE_ENV
+  )
+    ? `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBHOST}/${process.env.DBNAME}?retryWrites=true&w=majority`
+    : `mongodb://localhost:27017/${process.env.DBNAME}?retryWrites=true&w=majority`;
   mongoose
-    .connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASSWORD}@${process.env.DBHOST}/${process.env.DBNAME}?retryWrites=true&w=majority`, {
+    .connect(databaseUrl, {
       useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true
+      useNewUrlParser: true
     })
     .then(() => console.log("DB Connected!"))
     .catch(err => console.log(`DB Connection Error: ${err.message}`));
-  }
+};
 
 module.exports = {
-  connect,
+  connect
 };
